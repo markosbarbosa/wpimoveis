3471de82dbaf174a1736597b150143000e7a294d
Jonas Thomaz de Faria <jonasthomaz@gmail.com>
2013-08-03 17:25:26 -0300

    identacao

--- taxonomy-imovel_tipos.php
+++ taxonomy-imovel_tipos.php
@@ -13,10 +13,10 @@
 			while (have_posts()) : the_post(); ?>
 				<li>
 					<?php if ( has_post_thumbnail()) : ?>
-   			<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" >
-   <?php the_post_thumbnail(); ?>
-   </a>
- <?php endif; ?>
+   						<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" >
+   							<?php the_post_thumbnail(); ?>
+   						</a>
+ 					<?php endif; ?>
 					<h2><?php the_title(); ?></h2>
 					<h4>Posted on <?php the_time('F jS, Y') ?></h4>
 					<p><?php the_content(__('(more...)')); ?></p>
