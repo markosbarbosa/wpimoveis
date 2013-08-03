86f6a4dcc4a0a471dc5659326df3cbf045a1253a
Jonas Thomaz de Faria <jonasthomaz@gmail.com>
2013-08-03 14:18:41 -0300

    Formatacao básica da listagem de imoveis

--- taxonomy-imovel_tipos.php
+++ taxonomy-imovel_tipos.php
@@ -1,8 +1,41 @@
+<?php get_header(); ?>
+<div id="main">
+	<div id="content">
+		
+		<h1>Listagem de Imóveis</h1>
 
 
-<?php if ( have_posts() ) : while( have_posts() ) : the_post(); ?>
- 
-<div class="entry-title"><?php the_title(); ?></div>
-<div class="entry-content"><?php the_content(); ?></div>
- 
-<?php endwhile; endif; ?>
+		<?php if(have_posts())
+		{
+			?>
+			<ul class='imovel_lista'>
+			<?php
+			while (have_posts()) : the_post(); ?>
+				<li>
+					<?php if ( has_post_thumbnail()) : ?>
+   			<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" >
+   <?php the_post_thumbnail(); ?>
+   </a>
+ <?php endif; ?>
+					<h2><?php the_title(); ?></h2>
+					<h4>Posted on <?php the_time('F jS, Y') ?></h4>
+					<p><?php the_content(__('(more...)')); ?></p>
+				</li>
+			<?php
+			endwhile;
+			?>
+			</ul>
+			<?php
+		}else{
+			?>
+				<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+			<?php
+		}
+		?>
+		
+	</div>
+	<?php get_sidebar(); ?>
+</div>
+<div id="delimiter">
+</div>
+<?php get_footer(); ?>
